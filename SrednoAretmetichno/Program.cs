5c73e09d2cb47d7b6807e7c5a8d53d4284755619
Teodor168 <teodor16iliev@gmail.com>
2024-02-16 12:25:32 +0200

    Vtora proba

---
+++ SrednoAretmetichno/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SrednoAretmetichno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            byte newNum=byte.Parse(Console.ReadLine());
+            byte newNumber=byte.Parse(Console.ReadLine());
+            Console.WriteLine(newNum + newNumber);
+        }
+    }
+}
---
+++ Zad1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+
+namespace Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chislo ot 0 do 128");
+            byte number = byte.Parse(Console.ReadLine());
+            Console.WriteLine("dai chislo");
+            int num = byte.Parse(Console.ReadLine());
+            int counter = 0;
+            string container= null;
+            //do
+            //{
+
+            //    string word = Console.ReadLine();
+            //    counter++;
+            //    container += word + ", ";
+            //} while (counter < num);
+
+            //while (counter < num)
+            //{
+            //    string word = Console.ReadLine();
+            //    counter++;
+            //    container += word + "," + " ";
+            //}
+
+            for (int i = 0; i < num; i++)
+            {
+                string word = Console.ReadLine();
+                counter++;
+                container += word + ", ";
+            }
+            Console.WriteLine($"broqt na dumite: {counter}");
+            Console.WriteLine(container);
+        }
+    }
+}
